c9cb609177058f34741390e59469e8d66f09d159
J.M. Robin <johnmichaelrobin@gmail.com>
2023-03-04 23:44:12 +0800

    Refactor function for converting date and updating elements

--- js/main.js
+++ js/main.js
@@ -28,46 +28,55 @@ async function getUser(user = "octocat") {
     }
 }
 
-function convertDate(isoDate) {
-    const date = new Date(isoDate);
-    const options = {year: "numeric", month: "long", day: "numeric"}
-    let formattedDate = date.toLocaleDateString("en-us", options);
-    formattedDate = formattedDate.replace(",", "");
-    return formattedDate;
+const DATE_FORMAT_OPTIONS = {day: "numeric", month: "long", year: "numeric"};
+
+function convertDate(isoDate, options = DATE_FORMAT_OPTIONS) {
+    try {
+        const date = new Date(isoDate);
+        const formattedDate = date.toLocaleDateString("en-us", options).replace(",", "");
+        return `Joined ${formattedDate}`;
+    } catch (error) {
+        console.error("error");
+        return "Invalid Date";
+    }
 }
 
-function setContent(element, data) {
-    if (!data) {
-        element.innerText = "Not Available";
-    } else {
-        element.innerText = data;
+function updateElement(element, data) {
+    try {
+        const el = typeof element === "string" ? document.querySelector(element) : element;
+
+        switch (el.classList[0]) {
+            case ".js-bio":
+                el.innerText = data || "This profile has no bio";
+                break;
+            case ".js-username":
+                el.innerText = `@${data}`;
+                break;
+            case ".js-avatar":
+                el.src = data;
+                break;
+            default:
+                el.innerText = data || "Not Available";
+                break;
+        }
+    } catch (error) {
+        console.error(`Error updating the element: ${error}`);
     }
 }
 
 function setLink(element, data) {
+    const el = document.querySelector(element);
+
     if (!data) {
-        element.style.cursor = "not-allowed";
+        el.style.cursor = "not-allowed";
     } else {
-        element.href = data;
-        element.target = "_blank";
+        el.href = data;
+        el.target = "_blank";
     }
 }
 
 window.addEventListener("DOMContentLoaded", async () => {
-    const userAvatar = document.querySelector(".js-avatar");
-    const userPublicName = document.querySelector(".js-public-name");
-    const username = document.querySelector(".js-username");
-    const userJoinedDate = document.querySelector(".js-joined-date");
-    const userBio = document.querySelector(".js-bio");
-    const userRepos = document.querySelector(".js-repos");
-    const userFollowers = document.querySelector(".js-followers");
-    const userFollowing = document.querySelector(".js-following");
-    const userLocation = document.querySelector(".js-location");
-    const userWebsite = document.querySelector(".js-website");
-    const userTwitter = document.querySelector(".js-twitter");
-    const userCompany = document.querySelector(".js-company");
-
-    const data = await getUser();
+    const data = await getUser("robinjmm");
     console.log(data);
     const {
         avatar_url,
@@ -85,20 +94,20 @@ window.addEventListener("DOMContentLoaded", async () => {
         company
     } = data;
 
-    userAvatar.src = avatar_url;
-    username.innerText = `@${login}`
-    setContent(userPublicName, name);
-    setContent(userJoinedDate, convertDate(created_at));
-    setContent(userBio, bio);
-    setContent(userRepos, public_repos);
-    setContent(userFollowers, followers);
-    setContent(userFollowing, following);
-    setContent(userLocation, location);
-    setContent(userWebsite, blog);
-    setContent(userTwitter, twitter_username);
-    setContent(userCompany, company);
+    updateElement(".js-avatar", avatar_url);
+    updateElement(".js-username", login);
+    updateElement(".js-public-name", name);
+    updateElement(".js-joined-date", convertDate(created_at));
+    updateElement(".js-bio", bio);
+    updateElement(".js-repos", public_repos);
+    updateElement(".js-followers", followers);
+    updateElement(".js-following", following);
+    updateElement(".js-location", location);
+    updateElement(".js-website", blog);
+    updateElement(".js-twitter", twitter_username);
+    updateElement(".js-company", company);
 
-    setLink(username, html_url);
-    setLink(userWebsite, blog);
-    setLink(userTwitter, twitter_username);
+    setLink(".js-username", html_url);
+    setLink(".js-website", blog);
+    setLink(".js-twitter", twitter_username);
 });
